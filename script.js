7ffdff66a618999feb6069f41a3db811fadb5aec
Ju4n Avendanoa <juanfernando.avendanoa@gmail.com>
2023-06-18 13:43:34 -0500

    port

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const port = 3000;
+const port = 5000;
 
 // Configurar el directorio de archivos estáticos
 app.use(express.static(path.join(__dirname)));
